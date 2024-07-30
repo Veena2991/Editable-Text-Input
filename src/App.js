c6b478d366ec853af13c063fefa51e0493edf116
Veena2991 <veenabattul@gmail.com>
2024-07-30 12:04:28 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import './App.css'
+
+import EditableContainer from './components/EditableContainer'
+
+// Replace your code here
+const App = () => <EditableContainer />
+
+export default App
---
+++ src/components/EditableContainer/styledComponents.js
@@ -0,0 +1,56 @@
+import styled from 'styled-components'
+
+export const AppContainer = styled.div`
+  background-color: #f5d0fe;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
+export const CardContainer = styled.div`
+  background-color: #ffffff;
+  border-radius: 10px;
+  padding: 20px;
+  height: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+export const Heading = styled.h1`
+  color: #000000;
+  font-size: 20px;
+  font-family: 'Roboto';
+`
+
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`
+
+export const Paragraph = styled.p`
+  color: #323f4b;
+  font-size: 15px;
+  font-family: 'Roboto';
+`
+
+export const Input = styled.input`
+  height: 30px;
+  width: 250px;
+  outline: none;
+  margin-right: 20px;
+  padding: 10px;
+`
+
+export const Button = styled.button`
+  height: 30px;
+  width: 55px;
+  cursor: pointer;
+  background-color: #d946ef;
+  color: #ffffff;
+  border: none;
+  font-size: 13px;
+  font-family: 'Roboto';
+`
